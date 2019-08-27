bfd0209962aabc4166c8e74ff2b0d44428f24633
MysticBoy <mysticboy@live.com>
2019-08-27 17:34:37 +0800

    添加项目文件。

---
+++ MoDao2MiniGUI/Program.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MoDao2MiniGUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) Console.WriteLine("请输入墨刀路径");
+            string modaodir = args.Length > 0 ? args[0] : Console.ReadLine();
+            Console.WriteLine("墨刀路径" + modaodir);
+            string modaoindex = System.IO.Path.Combine(modaodir, "index.html");
+            if (!System.IO.File.Exists(modaoindex))
+            {
+                Console.WriteLine("未找到index.html");
+            }
+            else
+            {
+                string indexhtml = System.IO.File.ReadAllText(modaoindex);
+                string mbdatatag = "<script>window.MBData=";
+                string jsontemplates = indexhtml.Substring(indexhtml.IndexOf(mbdatatag) + mbdatatag.Length);
+                JObject jObject=JObject.Parse( jsontemplates.Remove(jsontemplates.IndexOf("</script>")));
+                var objs = jObject["widgets"].ToArray();
+      
+                string[] keynames = Properties.Resources.KeyName.Split('\r', '\n');
+                List<string> ctlcode = new List<string>();
+                List<string> ctlids = new List<string>();
+                int labelcount = 0;
+                foreach (var item in objs)
+                {
+                    var wt = item.ToObject<widget>();
+                    var keyname = from st in keynames where Check(wt, st) select st;
+                    var idname = keyname.Any() ? keyname.FirstOrDefault().Split(new char[] { ' ', (char)160 }, StringSplitOptions.RemoveEmptyEntries)[1] : string.Empty;
+                    if (string.IsNullOrEmpty(idname))
+                    {
+
+                    }
+                    switch (wt.name)
+                    {
+                        case "image_view":
+
+                            break;
+                        case "button":
+
+                            string buttoncode = $"{{CTRL_BUTTON, WS_VISIBLE | BS_PUSHBUTTON | WS_TABSTOP, {wt.left}, {wt.top}, {wt.width}, {wt.height}, ID_{idname}, \"{wt.text}\",0 }}";
+                            string keyvalue = Properties.Resources.KEYMAP_CT.Substring(Properties.Resources.KEYMAP_CT.IndexOf(idname));
+                            keyvalue = keyvalue.Substring(keyvalue.IndexOf("=<")+2, keyvalue.IndexOf(">") - keyvalue.IndexOf("=<")-2);
+                            ctlcode.Add(buttoncode);
+                            ctlids.Add($"#define ID_{idname} {1000+int.Parse( keyvalue)}");
+                            break;
+                        case "label":
+                            string lablecode = $"{{CTRL_STATIC, WS_VISIBLE | SS_CENTER, {wt.left},{wt.top},  {wt.width}, {wt.height}, ID_{wt.cid}, \"{wt.text}\",0 }}";
+                            ctlcode.Add(lablecode);
+                            labelcount++;
+                            ctlids.Add($"#define ID_{wt.cid} {1200 + labelcount}");
+                            break;
+                        default:
+                            Console.WriteLine(wt.name);
+                            break;
+                    }
+                }
+                string code = "";
+                foreach (var item in ctlids)
+                {
+                    code += item + "\r\n";
+                }
+                code +=  "\r\n";
+                code += "static CTRLDATA CtrlYourTaste[] = \r\n{";
+                for (int i = 0; i < ctlcode.Count; i++)
+                {
+                    code += "\r\n";
+                    code += ctlcode[i];
+                    if (i +1< ctlcode.Count)
+                    {
+                        code += ",";
+                    }
+                }
+                code += "};";
+
+
+
+            }
+        }
+
+        private static bool Check(widget wt, string st)
+        {
+            return !string.IsNullOrEmpty(st) && wt != null && !string.IsNullOrEmpty(wt.text) && st.Trim().Split(new char[] { ' ', (char)160 }, StringSplitOptions.RemoveEmptyEntries)[0].Equals(wt.text.Replace(" ", "").Replace("" + (char)160, "").Trim());
+        }
+    }
+}
---
+++ MoDao2MiniGUI/widgets.cs
@@ -0,0 +1,176 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoDao2MiniGUI
+{
+    public class widget
+    {
+        /// <summary>
+        /// w5F2C7C70FF1508909431298
+        /// </summary>
+        public string cid { get; set; }
+        /// <summary>
+        /// s2581663A031508909193242
+        /// </summary>
+        public string screen_cid { get; set; }
+        /// <summary>
+        /// Timestamp
+        /// </summary>
+        public int timestamp { get; set; }
+        /// <summary>
+        /// V
+        /// </summary>
+        public bool v { get; set; }
+        /// <summary>
+        /// button
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// Left
+        /// </summary>
+        public int left { get; set; }
+        /// <summary>
+        /// Top
+        /// </summary>
+        public int top { get; set; }
+        /// <summary>
+        /// Width
+        /// </summary>
+        public int width { get; set; }
+        /// <summary>
+        /// Height
+        /// </summary>
+        public int height { get; set; }
+        /// <summary>
+        /// Gid
+        /// </summary>
+        public string gid { get; set; }
+        /// <summary>
+        /// Locked
+        /// </summary>
+        public bool locked { get; set; }
+        /// <summary>
+        /// 下班 B
+        /// </summary>
+        public string text { get; set; }
+        /// <summary>
+        /// Fs
+        /// </summary>
+        public int fs { get; set; }
+        /// <summary>
+        /// Br
+        /// </summary>
+        public int br { get; set; }
+        /// <summary>
+        /// Bs
+        /// </summary>
+        public int bs { get; set; }
+        /// <summary>
+        /// center
+        /// </summary>
+        public string ha { get; set; }
+        /// <summary>
+        /// middle
+        /// </summary>
+        public string va { get; set; }
+        /// <summary>
+        /// Z
+        /// </summary>
+        public int z { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string th { get; set; }
+        /// <summary>
+        /// normal
+        /// </summary>
+        public string td { get; set; }
+        /// <summary>
+        /// Bo
+        /// </summary>
+        public int bo { get; set; }
+        /// <summary>
+        /// I
+        /// </summary>
+        public int i { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string bg { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string bc { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string tc { get; set; }
+        /// <summary>
+        /// O
+        /// </summary>
+        public int o { get; set; }
+        /// <summary>
+        /// Ts
+        /// </summary>
+        public int ts { get; set; }
+        /// <summary>
+        /// #aaaaaa
+        /// </summary>
+        public string tsc { get; set; }
+        /// <summary>
+        /// Ds
+        /// </summary>
+        public int ds { get; set; }
+        /// <summary>
+        /// #aaaaaa
+        /// </summary>
+        public string sc { get; set; }
+        /// <summary>
+        /// Ro
+        /// </summary>
+        public int ro { get; set; }
+        /// <summary>
+        /// Ls
+        /// </summary>
+        public int ls { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string icon { get; set; }
+        /// <summary>
+        /// Is
+        /// </summary>\
+        [JsonProperty("is")]
+        public int is_ { get; set; }
+        /// <summary>
+        /// Fixed
+        /// </summary>
+        [JsonProperty("fixed")]
+        public bool fixed_ { get; set; }
+        /// <summary>
+        /// Fixed_type
+        /// </summary>
+        public int fixed_type { get; set; }
+        /// <summary>
+        /// none
+        /// </summary>
+        public string ani_name { get; set; }
+        /// <summary>
+        /// 0
+        /// </summary>
+        public string ani_delay { get; set; }
+        /// <summary>
+        /// 1
+        /// </summary>
+        public string ani_count { get; set; }
+        /// <summary>
+        /// 0
+        /// </summary>
+        public string ani_duration { get; set; }
+    }
+
+}
+ 
